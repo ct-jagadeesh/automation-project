4a5dfcafddf5f1906263ff852a3e9c780a09a3db
ct-jagadeesh <jagadeesh.a@cartrade.com>
2026-01-08 11:07:47 +0530

    Fix CI: enforce headless mode for Playwright

--- utils/driver.py
+++ utils/driver.py
@@ -1,14 +1,11 @@
 from playwright.sync_api import sync_playwright
 
-def start_browser(headless=False):
+def start_browser(headless=True):
     pw = sync_playwright().start()
     browser = pw.chromium.launch(headless=headless)
     page = browser.new_page()
     return pw, browser, page
 
 def close_browser(pw, browser):
-    try:
-        browser.close()
-        pw.stop()
-    except:
-        pass
+    browser.close()
+    pw.stop()
